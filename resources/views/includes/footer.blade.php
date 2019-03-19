93b83f4e336e80d82ae7b6960046124db348d1b5
haisam <he3@njit.edu>
2019-03-18 21:42:57 -0400

    set the authentication

---
+++ resources/views/includes/footer.blade.php
@@ -0,0 +1,3 @@
+<footer class="container">
+    <p>&copy; 19 S - IS 601102-Web Systems Development</p>
+</footer>
---
+++ resources/views/includes/menu.blade.php
@@ -0,0 +1,30 @@
+<nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+    <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+                <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="/about">About</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="/contact">Contact </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="/tickets/show">Ticket </a>
+            </li>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="http://example.com" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">User Account</a>
+                <div class="dropdown-menu" aria-labelledby="dropdown01">
+                    <a class="dropdown-item" href="/login">Login</a>
+                    <a class="dropdown-item" href="/register">Rregister</a>
+                    <a class="dropdown-item" href="/index">Show</a>
+                </div>
+            </li>
+        </ul>
+        <form class="form-inline my-2 my-lg-0">
+            <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+        </form>
+    </div>
+</nav>
